edfeecb998ad328104ce5dda0eb5c37954c2099b
root <root@F003126.finnet.corp>
2019-06-21 18:00:45 -0300

    Piper e Resolver

---
+++ src/app/photos/photo-list/phot-list.resolver.ts
@@ -0,0 +1,17 @@
+import { PhotoService } from '../photo/photo.service';
+import { Injectable } from '@angular/core';
+import { Resolve } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Photo } from '../photo/photo';
+
+@Injectable({ providedIn: 'root' })
+export class PhotListResolver implements Resolve<Observable<Photo[]>>{
+
+    resolve(ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Photo[]> | Observable<Observable<Photo[]>> | Promise<Observable<Photo[]>> {
+        throw new Error('Method not implemented.');
+    }
+
+    constructor(private service: PhotoService) {
+
+    }
+}
--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { PhotoService } from '../photo/photo.service';
+import { Photo } from '../photo/photo';
 
 @Component({
   selector: 'app-photo-list',
@@ -10,22 +11,22 @@ import { PhotoService } from '../photo/photo.service';
 })
 export class PhotoListComponent implements OnInit {
 
-  photos: any[] = []; 
-  
+  photos: Photo[] = [];
+  filter: String = '';
   constructor(
     private photoService: PhotoService,
     private activatedRoute: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
-    
-      const userName = this.activatedRoute
-        .snapshot
-        .params
-        .userName;
 
-      this.photoService
-        .listFromUser(userName)
-        .subscribe(photos => this.photos = photos);
+    const userName = this.activatedRoute
+      .snapshot
+      .params
+      .userName;
+
+    this.photoService
+      .listFromUser(userName)
+      .subscribe(photos => this.photos = photos);
   }
 }
