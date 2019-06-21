998fdc19d5f186202f6b8850e3b0832eae9ae146
root <root@F003126.finnet.corp>
2019-06-21 13:14:46 -0300

    Update Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { PhotoService } from './photos/photo/photo.service';
-import { Photo } from './photos/photo/photo';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-
-  photos: any[] = [];
-
-  constructor(private photoService: PhotoService) {
-  }
-
-  ngOnInit(): void {
-    this.photoService.listFromUser('flavio').subscribe(photos => {
-      console.log(photos[0].description);
-      this.photos = photos
-    });
-  }
-
-}
+export class AppComponent { }
